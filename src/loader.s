b6fb6ecb82be55e298f0a021d502f2beb3a54b7c
Woundrite <ndbendale2004@gmail.com>
2021-05-31 11:07:46 +0530

    initial boot

---
+++ src/loader.s
@@ -0,0 +1,31 @@
+.set BOOTIDENTIFIER, 0x1badb002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(BOOTIDENTIFIER + FLAGS)
+
+.section .multiboot
+    .long BOOTIDENTIFIER
+    .long FLAGS
+    .long CHECKSUM
+
+.section .text
+.extern KernelBoot
+.extern callConstructors
+.global loader
+
+loader:
+    mov $Kernel_Stack, %esp
+
+    call callConstructors
+
+    push %eax
+    push %ebx
+    call KernelBoot
+_stop:
+    cli
+    hlt
+    jmp _stop
+
+.section .bss
+.space 2*1024*1024 ; # 2MB
+
+Kernel_Stack:
