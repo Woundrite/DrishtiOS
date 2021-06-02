3cf9875079ad081277a0c964b33a4f4ed8c34ae0
Woundrite <ndbendale2004@gmail.com>
2021-06-02 14:25:44 +0530

    Editted Keyboard Functionality(for science purposes) pt3

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -25,6 +25,114 @@ uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp){
             case 0xC5:
             case 0xFA:
                 break;
+            case 0x1E:
+                printf("A");
+                break;
+            case 0x30:
+                printf("B");
+                break;
+            case 0x2E:
+                printf("C");
+                break;
+            case 0x20:
+                printf("D");
+                break;
+            case 0x12:
+                printf("E");
+                break;
+            case 0x21:
+                printf("F");
+                break;
+            case 0x22:
+                printf("G");
+                break;
+            case 0x23:
+                printf("H");
+                break;
+            case 0x17:
+                printf("I");
+                break;
+            case 0x24:
+                printf("J");
+                break;
+            case 0x25:
+                printf("K");
+                break;
+            case 0x26:
+                printf("L");
+                break;
+            case 0x32:
+                printf("M");
+                break;
+            case 0x31:
+                printf("N");
+                break;
+            case 0x18:
+                printf("O");
+                break;
+            case 0x19:
+                printf("P");
+                break;
+            case 0x10:
+                printf("Q");
+                break;
+            case 0x13:
+                printf("R");
+                break;
+            case 0x1F:
+                printf("S");
+                break;
+            case 0x14:
+                printf("T");
+                break;
+            case 0x16:
+                printf("U");
+                break;
+            case 0x2F:
+                printf("V");
+                break;
+            case 0x11:
+                printf("W");
+                break;
+            case 0x2D:
+                printf("X");
+                break;
+            case 0x15:
+                printf("Y");
+                break;
+            case 0x2C:
+                printf("Z");
+                break;
+            case 0x02:
+                printf("1");
+                break;
+            case 0x03:
+                printf("2");
+                break;
+            case 0x04:
+                printf("3");
+                break;
+            case 0x05:
+                printf("4");
+                break;
+            case 0x06:
+                printf("5");
+                break;
+            case 0x07:
+                printf("6");
+                break;
+            case 0x08:
+                printf("7");
+                break;
+            case 0x09:
+                printf("8");
+                break;
+            case 0x0A:
+                printf("9");
+                break;
+            case 0x0B:
+                printf("0");
+                break;
             default:
                 char* foo = "KEYBOARD 0x00";
                 char* hex = "0123456789ABCDEF";
