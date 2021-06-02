5cd3592e91696b00b183aa07274397428976f0b6
Woundrite <ndbendale2004@gmail.com>
2021-06-02 22:06:39 +0530

    Trying to figure out what is wrong with mouse pt 5

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -6,7 +6,9 @@ MouseDriver::MouseDriver(InterruptManager* Manager):InterruptHandler(0x2C, Manag
     buttons = 0;
 
     uint16_t* VideoMemory = (uint16_t*)0xb8000;
-    VideoMemory[80*12+40] = ((VideoMemory[80*12+40] & 0x0F00) >> 4) | ((VideoMemory[80*12+40] & 0xF000) << 4) | ((VideoMemory[80*12+40] & 0x00FF));
+    VideoMemory[80*12+40] = (VideoMemory[80*12+40] & 0x0F00) << 4
+                                    | (VideoMemory[80*12+40] & 0xF000) >> 4
+                                    | (VideoMemory[80*12+40] & 0x00FF);
 
     CommandPort.Write(0xA8); // activate interrupts
     CommandPort.Write(0x20); // gget current state of keyboard
@@ -40,7 +42,9 @@ uint32_t MouseDriver::HandleInterrupt(uint32_t esp){
 
         static uint16_t* VideoMemory = (uint16_t*)0xb8000;
         
-        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0x0F00) >> 4) | ((VideoMemory[80*y+x] & 0xF000) << 4) | ((VideoMemory[80*y+x] & 0x00FF));
+        VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
+                                    | (VideoMemory[80*y+x] & 0xF000) >> 4
+                                    | (VideoMemory[80*y+x] & 0x00FF);
 
         x += buffer[1];
 
@@ -56,7 +60,9 @@ uint32_t MouseDriver::HandleInterrupt(uint32_t esp){
 
         y -= buffer[2];
 
-        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4) | ((VideoMemory[80*y+x] & 0xF000) << 4) | ((VideoMemory[80*y+x] & 0x00FF));
+        VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0x0F00) << 4
+                                    | (VideoMemory[80*y+x] & 0xF000) >> 4
+                                    | (VideoMemory[80*y+x] & 0x00FF);
     }
 
     return esp;
