649192b3d152f273cf66220df798c0e9dba220c8
Woundrite <ndbendale2004@gmail.com>
2021-06-08 12:43:39 +0530

    removed the experimental files

--- src/testing/include/inc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-void print(char* str);
-void print(char c);
-void print(int i);
-void print(float f);
-void print(double d);
--- src/testing/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "include/tst/test.h"
-
-int main(){
-    print("STR");
-    print("C");
-    print(1);
-    print(1.4);
-}
