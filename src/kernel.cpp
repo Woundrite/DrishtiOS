ffc34dd810345f0da2134a91436718c40a3d2b55
Woundrite <ndbendale2004@gmail.com>
2021-06-10 15:43:26 +0530

    blue box GOOOO

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -139,8 +139,8 @@ namespace Drishti{
         Interrupts.Activate();
 
         vga.SetMode(320, 200, 8);
-        for(Types::uint8_t y = 0; y < 200; y++)
-            for(Types::uint8_t x = 0; x < 320; x++)
+        for(Types::int32_t y = 0; y < 200; y++)
+            for(Types::int32_t x = 0; x < 320; x++)
                 vga.PutPixel(x, y, 0x00, 0x00, 0xA8);
 
         while(1);
