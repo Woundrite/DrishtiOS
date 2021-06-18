cc27b29c84650117b01c980605a2d0023da165e6
Woundrite <ndbendale2004@gmail.com>
2021-06-18 10:13:39 +0530

    reverting changes to wrong branches

--- src/loader.s
+++ src/loader.s
@@ -10,52 +10,23 @@
 .section .text
 .extern KernelBoot
 .extern callConstructors
+.global loader
 
-mov eax, cr4
-or eax, 0x20
-mov cr4, eax
+loader:
+    mov $Kernel_Stack, %esp
 
-mov edi, 0x300000
+    call callConstructors
 
-mov eax, 0x30100F
-stosd
-mov ecx, 0x03FF
-xor eax, eax
-rep stosd
+    push %eax
+    push %ebx
+    call KernelBoot
 
-mov eax, 0x30200F
-stosd
-mov ecx, 0x03FF
-xor eax, eax
-rep stosd
+_stop:
+    cli
+    hlt
+    jmp _stop
 
-mov ecx, 0x0200
-mov esi, 0x008B
+.section .bss
+.space 2*1024*1024 ; # 2MB
 
-map.loop: 
-mov eax, esi
-stosd
-xor eax, eax
-stosd
-add esi, 0x200000
-sub ecx, 1
-jnz map.loop
-
-mov eax, 0x300000
-mov cr3, eax
-
-mov ecx, 0xC0000080
-rdmsr
-or eax, 0x00000100
-wrmsr
-
-mov eax, cr0
-or eax, 0x80000000
-mov cr0, eax
-jmp 0x30 kernel.long mode
-
-
-[bits 64]
-kernel.long mode:
-call KernelBoot
-jmp $ 
+Kernel_Stack:
